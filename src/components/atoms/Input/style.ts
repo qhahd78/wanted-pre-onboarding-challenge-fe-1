0a89cdb9e2033e812992ff04f3082f852c8b33d2
qhahd78 <umuj0708@likelion.org>
2022-08-04 18:37:08 +0900

    Chore:base setting

---
+++ src/components/atoms/Input/style.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const TestBox = styled.div``;
+
+export { TestBox };
---
+++ src/styles/global/index.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    html, 
+    body {
+        /* pretendard 교체 예정 */
+        font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+        font-size: 16px;
+        font-weight: 500;
+        /* white 임시 */
+        background-color: white; 
+        ::-webkit-scrollbar{ display:none; }
+        overflow-x: hidden;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    a,
+    a:link,
+    a:visited,
+    a:hover{
+        text-decoration: none;
+        color: inherit;
+    }
+    input{
+         font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+        padding: 0;
+        margin: 0;
+    }
+    textarea {
+         font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+    }
+    button{
+         font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+         background-color: transparent;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        padding: 0;
+    }
+`;
+
+export default GlobalStyle;
